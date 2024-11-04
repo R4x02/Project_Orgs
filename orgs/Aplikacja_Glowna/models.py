c4b2864abd37fcd7106ce9ceee8cdbfdec4baad7
Oskar Topolewicz <oskar@Oskars-MacBook-Air.local>
2024-11-04 11:18:34 +0100

    drugi commit

--- orgs/Aplikacja_Glowna/models.py
+++ orgs/Aplikacja_Glowna/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import ForeignKey
+
 
 # Create your models here.
+class Pacjent(models.Model):
+    Nazwisko = models.CharField(max_length=100)
+    Imie = models.CharField(max_length=100)
+    Miasto = models.CharField(max_length=100, default='Białystok')
+    Ulica = models.CharField(max_length=100)
+    Wiek = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Imie} {self.Nazwisko}"
+
+class Wizyta(models.Model):
+    Data = models.DateField()
+    Zalecenia = models.TextField()
+    pacjent_klucz_obcy = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.Data} {self.pacjent_klucz_obcy.Nazwisko}"
--- orgs/Aplikacja_Glowna/views.py
+++ orgs/Aplikacja_Glowna/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Wizyta, Pacjent
 
-# Create your views here.
+def main(request):
+    WIZYTY = Wizyta.objects.count()
+    PACJENCI = Pacjent.objects.all()
+    return render(request, 'index.html', {'ilosc_wizyt': WIZYTY, 'pacjenci': PACJENCI})
