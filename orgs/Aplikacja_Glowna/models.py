2b9109f61b1376ba7e004bb78b3b7bc4ded0cc96
Jakub-Zawadzki <jakub.zawadzki@infotech.edu.pl>
2024-11-04 11:37:25 +0100

    models,views i html

--- orgs/Aplikacja_Glowna/models.py
+++ orgs/Aplikacja_Glowna/models.py
@@ -3,20 +3,13 @@ from django.db.models import ForeignKey
 
 
 # Create your models here.
-class Pacjent(models.Model):
-    Nazwisko = models.CharField(max_length=100)
+class Uzytkownik(models.Model):
     Imie = models.CharField(max_length=100)
-    Miasto = models.CharField(max_length=100, default='Białystok')
-    Ulica = models.CharField(max_length=100)
-    Wiek = models.IntegerField()
+    Nazwisko = models.CharField(max_length=100)
+    gmail = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.Imie} {self.Nazwisko}"
 
-class Wizyta(models.Model):
-    Data = models.DateField()
-    Zalecenia = models.TextField()
-    pacjent_klucz_obcy = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.Data} {self.pacjent_klucz_obcy.Nazwisko}"
+
--- orgs/Aplikacja_Glowna/views.py
+++ orgs/Aplikacja_Glowna/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from .models import Wizyta, Pacjent
+from .models import Uzytkownik
 
 def main(request):
-    WIZYTY = Wizyta.objects.count()
-    PACJENCI = Pacjent.objects.all()
-    return render(request, 'index.html', {'ilosc_wizyt': WIZYTY, 'pacjenci': PACJENCI})
+    UZYTKOWNIK = Uzytkownik.objects.count()
+    return render(request, 'index.html', {'ilosc_uzytkownikow': UZYTKOWNIK})
