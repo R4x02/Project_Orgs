bb98ee50ccff90970e285b56933aa7ef9786891c
_SMoody <maciej.mocarski@infotech.edu.pl>
2024-11-29 10:30:37 +0100

    zmiany widokowe i css'owe, dodanie nie dzialającego w pelni darkmode'a

--- orgs/orgs/static/code.js
+++ orgs/orgs/static/code.js
@@ -3,11 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     teamLinks.forEach(link => {
         link.addEventListener('mouseover', function() {
-            this.closest('.team-name').style.backgroundColor = 'lightblue';
+            this.closest('.team-item').style.backgroundColor = '#f5f5f5';
         });
 
         link.addEventListener('mouseout', function() {
-            this.closest('.team-name').style.backgroundColor = '';
+            this.closest('.team-item').style.backgroundColor = '';
         });
     });
 });
+
+function darkliteFunc() {
+    document.querySelectorAll('*:not(item-name)').forEach(element => {
+        element.classList.toggle('dark-mode');
+    });
+
+    if (document.querySelector('body').classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.removeItem('darkMode');
+    }
+}
